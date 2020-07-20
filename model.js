7c61b5aa9bb01144e7473a48a5eae1acca673ae2
onurkybsi <onurbpm@outlook.com>
2020-07-20 23:24:41 +0300

    Model architecture changed

--- model.js
+++ model.js
@@ -1,43 +1,28 @@
-const validateAddProperty = require("./modelValidations");
+const modelValidations = require("./modelValidations");
 
-function Model() {
-  this.properties = {};
-  this.isValid = false;
+class Model {
+  constructor() {
+    this.properties = {};
+    this.isValid = false;
+  }
+  addProperty = function (propertyName, propertyType, propertyRules) {
+    let parametersIsValid = modelValidations.validateAddProperty(
+      propertyName,
+      propertyType,
+      propertyRules
+    );
+    if (!parametersIsValid.isValid) {
+      throw Error(parametersIsValid.errorMessage);
+    }
+
+    this.properties = {
+      ...this.properties,
+      [propertyName]: {
+        propertyType: propertyType,
+        propertyRules: propertyRules,
+      },
+    };
+  };
 }
 
-Model.prototype.addProperty = function (
-  propertyName,
-  propertyType,
-  propertyRules
-) {
-  let parametersIsValid = validateAddProperty(
-    propertyName,
-    propertyType,
-    propertyRules
-  );
-  if (!parametersIsValid.isValid) {
-    throw Error(parametersIsValid.errorMessage);
-  }
-
-  this.properties = {
-    ...this.properties,
-    [propertyName]: {
-      propertyType: propertyType,
-      propertyRules: propertyRules,
-    },
-  };
-};
-
-exports.createModel = function () {
-  const model = new Model();
-
-  for (const prop in model) {
-    Object.defineProperty(model, prop, {
-      writable: false,
-      configurable: false,
-    });
-  }
-  Object.preventExtensions(model);
-
-  return model;
-};
+exports.createModel = () => new Model();
